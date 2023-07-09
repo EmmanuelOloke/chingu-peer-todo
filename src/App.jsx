4bf680c31f5770f7261d23e841b042f44f92f854
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-09 19:22:13 +0100

    Feat: Created Todo styles and implemented the Add Todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+
+import { Button, Flex, HStack, Heading, Input, Text, VStack } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputText, setInputText] = useState('');
+  const [todoItems, setTodoItems] = useState(
+    () => JSON.parse(localStorage.getItem('todoItems')) || []
+  );
+
+  const addTodo = () => {
+    const updatedArray = [...todoItems, inputText];
+    setTodoItems(updatedArray);
+    setInputText('');
+  };
+
+  const updateInput = (e) => {
+    setInputText(e.target.value);
+  };
+
+  useEffect(() => {
+    localStorage.setItem('todoItems', JSON.stringify(todoItems));
+  }, [todoItems]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Flex h="100vh" justifyContent="center" alignItems="center">
+      <VStack border="1px solid gray" borderRadius="8px" p={5}>
+        <Heading>Todo App</Heading>
+
+        {todoItems.length === 0 ? (
+          <Text>No items added to the todo yet</Text>
+        ) : (
+          todoItems.map((eachTodoItem) => (
+            <Text borderBottom="2px solid blue" key={eachTodoItem}>
+              {eachTodoItem}
+            </Text>
+          ))
+        )}
+
+        <HStack>
+          <Input
+            type="text"
+            placeholder="Enter a todo task"
+            value={inputText}
+            onChange={updateInput}
+          />
+          <Button
+            size="lg"
+            bgColor="skyblue"
+            color="white"
+            _hover={{ bgColor: 'black' }}
+            onClick={addTodo}
+          >
+            ADD TASK
+          </Button>
+        </HStack>
+      </VStack>
+    </Flex>
+  );
 }
 
-export default App
+export default App;
