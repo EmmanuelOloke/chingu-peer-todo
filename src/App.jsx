b46e94bbeede2b4e39568601dd17719309d87199
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-07-15 17:53:23 +0100

    feat: add date field and trigger addTodo when the Enter key is pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Button, HStack, Heading, Input, StackDivider, Text, VStack } from '@chakra-ui/react';
 import { DeleteIcon, CheckIcon, MinusIcon } from '@chakra-ui/icons';
@@ -14,6 +14,7 @@ function App() {
   const todo = {
     id: nanoid(),
     body: inputText,
+    date: `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`,
     isDone: false,
   };
 
@@ -37,6 +38,12 @@ function App() {
     setTodoItems(newTodoItems);
   };
 
+  const addTodoButtonRef = useRef();
+  const enterKeyPressed = (event) => {
+    if (event.key === 'Enter') {
+      addTodoButtonRef.current.click();
+    }
+  };
   useEffect(() => {
     localStorage.setItem('todoItems', JSON.stringify(todoItems));
   }, [todoItems]);
@@ -44,7 +51,13 @@ function App() {
   return (
     <VStack h="100vh" justifyContent="center" alignItems="center">
       <Heading>Todo App</Heading>
-      <VStack divider={<StackDivider />} border="1px solid gray" borderRadius="8px" p={5}>
+      <VStack
+        divider={<StackDivider />}
+        border="1px solid gray"
+        borderRadius="8px"
+        p={5}
+        w={{ base: '90%', lg: '40%' }}
+      >
         {todoItems.length === 0 ? (
           <Text>No items added to the todo yet</Text>
         ) : (
@@ -56,9 +69,14 @@ function App() {
                 <MinusIcon cursor="pointer" onClick={() => checked(index)} />
               )}
 
-              <Text style={{ textDecoration: eachTodoItem.isDone ? 'line-through' : '' }}>
+              <Text
+                maxWidth="17rem"
+                style={{ textDecoration: eachTodoItem.isDone ? 'line-through' : '' }}
+              >
                 {eachTodoItem.body}
               </Text>
+
+              <Text w="5rem">{eachTodoItem.date}</Text>
               <DeleteIcon cursor="pointer" onClick={() => deleted(eachTodoItem.id)} />
             </HStack>
           ))
@@ -70,8 +88,10 @@ function App() {
             placeholder="Enter a todo task"
             value={inputText}
             onChange={updateInput}
+            onKeyDown={enterKeyPressed}
           />
           <Button
+            ref={addTodoButtonRef}
             size="lg"
             bgColor="skyblue"
             color="white"
