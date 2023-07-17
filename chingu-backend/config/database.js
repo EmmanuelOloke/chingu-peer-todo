3dd763bda7207b860ed45b67130d784234639599
Braine Lomoni <82029545+code-briomar@users.noreply.github.com>
2023-07-17 13:00:33 +0300

    feat : Setup up the database configuration file

---
+++ chingu-backend/config/database.js
@@ -0,0 +1,30 @@
+import sqlite3 from "sqlite3"
+
+export const connect_to_database = () =>{
+
+    try{
+        //Path to the database
+        const database_name = "chingu_db.db";
+        const database_path = `../database/${database_name}`;
+
+        //Database connection
+        const chingu_db = new sqlite3.Database(database_path,(error)=>{
+        if(error){
+            console.error(error.message)
+        } else {
+            console.log("Connected to DB.")
+        }
+        })
+
+        //Close database connection
+        chingu_db.close((error)=>{
+        if(error){
+            console.error(error.message)
+        } else {
+            console.log("Disconnected.")
+        }
+        })
+    }catch(error){
+        console.error(error.message)
+    }
+}
