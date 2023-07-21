b12da95a2d9010f6c93590fc7c047c2f9a29c800
Braine Lomoni <kapolonbraine@gmail.com>
2023-07-21 20:12:18 +0300

    feat : Setup middleware for todo route

--- chingu-backend/index.js
+++ chingu-backend/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connect_to_database } from './config/database.js';
 import morgan from 'morgan';
+import router from "./routes/todoRoutes.js";
 dotenv.config();
 
 const app = express();
 //Setup morgan to log API status
 app.use(morgan('dev'))
+
+
+//Setup middleware for the /todo route
+app.use("/todo", router);
 
 const PORT = process.env.PORT;
 
