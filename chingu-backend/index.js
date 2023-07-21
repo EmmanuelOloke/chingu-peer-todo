c68a24ff9e9cd5db1fa59cf6d447c01c84a07d27
Braine Lomoni <kapolonbraine@gmail.com>
2023-07-21 19:32:43 +0300

    feat : Setup morgan to log API status

--- chingu-backend/index.js
+++ chingu-backend/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connect_to_database } from './config/database.js';
+import morgan from 'morgan';
 dotenv.config();
 
 const app = express();
-
+//Setup morgan to log API status
+app.use(morgan('dev'))
 
 const PORT = process.env.PORT;
 
