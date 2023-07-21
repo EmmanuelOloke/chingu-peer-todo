e0cc44d0c4c33e7ce6f3d6d97056c3c766975ff2
Braine Lomoni <kapolonbraine@gmail.com>
2023-07-21 19:30:41 +0300

    feat : Import connect_to_database function and called it once server runs

--- chingu-backend/index.js
+++ chingu-backend/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import { connect_to_database } from './config/database.js';
 dotenv.config();
 
 const app = express();
 
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
+  connect_to_database();
   console.log(`Server is running on port ${PORT}`);
 });
